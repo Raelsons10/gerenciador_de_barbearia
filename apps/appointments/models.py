5d1596fe26e398add1f1787773e4953cbd60dcaa
Raelson Santos <raelsons45@gmail.com>
2026-06-01 16:54:51 -0300

    refactor: improve appointment schedulling model

--- apps/appointments/models.py
+++ apps/appointments/models.py
@@ -1,8 +1,14 @@
+import datetime
 from django.db import models
+from django.db.models import Sum
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
 from django.utils import timezone
 from apps.clients.models import Client
 from apps.services.models import Service
 from apps.employees.models import Employee
+
+
 
 class StatusAppointment(models.TextChoices):
     AGENDADO = 'scheduled', 'Agendado'
@@ -14,27 +20,32 @@ class Appointment(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True)
     services = models.ManyToManyField(Service)
     status = models.CharField(choices=StatusAppointment.choices, default=StatusAppointment.AGENDADO, max_length=50)
-    time = models.DateTimeField(verbose_name='Data do agendamento', default=timezone.now, blank=False, null=False)
     observation = models.TextField(verbose_name='Observações', blank=True)
 
-    # Coletando dados da duração do agendamento
+    # coletando dados do início e fim do agendamento.
     start_time = models.DateTimeField()
-    duration = models.DurationField()
-    end_time = models.DateTimeField(blank=True, null=True)
+    end_time = models.DateTimeField(blank=True, null=True, editable=False)
 
-    def save(self, *args, **kwargs):
-        if self.start_time and self.duration:
-            self.end_time = self.start_time + self.duration
-
-        super().save(*args, **kwargs)
-
-
+    # Coletando dados da duração do agendamento.
+    total_duration = models.DurationField(default=datetime.timedelta, editable=False)
     class Meta:
         verbose_name = "Agendamento"
         verbose_name_plural = "Agendamentos"
-        ordering = ['-time']
+        ordering = ['-start_time']
 
     def __str__(self):
-        return f"{self.client} - {self.time.strftime('%d/%m/%Y %H:%M')}"
+        return f"{self.client} - {self.start_time.strftime('%d/%m/%Y %H:%M')}"
     
+    # Signals para calcular automaticamente a duração total de um agendamento.
+@receiver(m2m_changed, sender=Appointment.services.through)
+def calculate_sum_services(sender, instance, action, **kwargs):
+    if action in ["post_add", "post_remove", "post_clear"]:
+        result = instance.services.aggregate(total_time=Sum('duration'))
+        instance.total_duration = result['total_time'] or datetime.timedelta()
 
+        if instance.start_time:
+            instance.end_time = instance.start_time + instance.total_duration
+        else:
+            instance.end_time = None
+
+        instance.save(update_fields=['total_duration', 'end_time'])
